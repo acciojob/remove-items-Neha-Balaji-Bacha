c71596df726c0b58d75c4a42621fb7e75e91541a
Neha-Balaji-Bacha <neha170603@gmail.com>
2025-11-15 15:47:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+document.getElementById("removebtn").addEventListener("click",()=>{
+	const option = document.getElementById("colorSelect");
+	if(option.selectedIndex !== -1){
+		option.remove(option.selectedIndex);
+	}
+});
